953ddc2c09e1209fa12f428e4effc9960983f331
Brandon Wolf <112419054+BMWolf57@users.noreply.github.com>
2023-03-21 16:40:18 -0400

    Update rgb_to_hex.java
    Added comments to rgb_to_hex.java

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -1,13 +1,16 @@
 public class rgb_to_hex {
     public static void main(String[] args) {
+        //intialize rgb values
         int r = 255;
         int g = 127;
         int b = 0;
+        //pass values in to rbgToHex
         String hexColor = rgbToHex(r, g, b);
         System.out.println("RGB color (" + r + ", " + g + ", " + b + ") = " + hexColor);
     }
 
     public static String rgbToHex(int r, int g, int b) {
+        //change these to hexadicemal and return the string
         r = Math.min(255, Math.max(0, r));
         g = Math.min(255, Math.max(0, g));
         b = Math.min(255, Math.max(0, b));
